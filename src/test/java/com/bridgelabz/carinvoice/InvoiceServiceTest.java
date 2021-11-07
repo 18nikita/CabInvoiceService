e16b652ea074940e9f6dee384dd7fdfced484cef
unknown <rajputnikita5666@gmail.com>
2021-11-07 14:31:09 +0530

    Added:Multiple ride for cab invoice

--- src/test/java/com/bridgelabz/carinvoice/InvoiceServiceTest.java
+++ src/test/java/com/bridgelabz/carinvoice/InvoiceServiceTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 
 public class InvoiceServiceTest {
 
+	//test case for calculate fare
 	@Test
 	public void givenDistanceAndTimeShouldReturnTotalFare() {
 		InvoiceService invoiceservice = new InvoiceService();
@@ -14,5 +15,17 @@ public class InvoiceServiceTest {
 
 		double fare = invoiceservice.calculateFare(distance, time);
 		Assert.assertEquals(25, fare, 0);
+	}
+	
+	//test case for multiple ride
+	@Test
+	public void givenDistanceAndTimeShouldReturnMinFare() {
+		InvoiceService invoiceservice = new InvoiceService();
+
+		double distance = 0.1;
+		int time = 1;
+
+		double fare = invoiceservice.calculateFare(distance, time);
+		Assert.assertEquals(5, fare, 0);
 	}
 }
